09b05bad6877f9a5eb01529e7ff9fb7fbc2d6e70
wenge8n <wenge8n@outlook.com>
2023-12-06 18:18:21 +0800

    Add video data output

--- DtexCamera/Classes/DtexCameraViewController.swift
+++ DtexCamera/Classes/DtexCameraViewController.swift
@@ -10,13 +10,16 @@ import AVFoundation
 
 open class DtexCameraViewController: UIViewController {
     
-    let captureSession = AVCaptureSession()
-    var stillImageOutput: AVCapturePhotoOutput!
-    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    private let captureSession = AVCaptureSession()
+    private var captureDevice: AVCaptureDevice!
+    private var stillImageOutput: AVCapturePhotoOutput!
+    private var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    private var videoDataOutput: AVCaptureVideoDataOutput!
+    private var videoDataOutputQueue: DispatchQueue!
 
     open override func viewDidLoad() {
         super.viewDidLoad()
-        configure()
+        configureSession()
     }
     
     open override func viewWillDisappear(_ animated: Bool) {
@@ -24,25 +27,41 @@ open class DtexCameraViewController: UIViewController {
         captureSession.stopRunning()
     }
     
-    private func configure() {
+    private func configureSession() {
         // Preset the session for taking photo in full resolution
         captureSession.sessionPreset = AVCaptureSession.Preset.photo
         
         // Get the back camera
-        guard let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) else {
+        guard let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) else {
+            return
+        }
+        captureDevice = device
+        
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else {
             return
         }
         
-        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: backCamera) else {
-            return
+        if captureSession.canAddInput(captureDeviceInput) {
+            captureSession.addInput(captureDeviceInput)
         }
         
         // Configure the session with the output for capturing still images
         stillImageOutput = AVCapturePhotoOutput()
         
-        // Configure the session with the input and the output devices
-        captureSession.addInput(captureDeviceInput)
-        captureSession.addOutput(stillImageOutput)
+        if captureSession.canAddOutput(stillImageOutput) {
+            captureSession.addOutput(stillImageOutput)
+        }
+        
+        // Configure video data output
+        videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        videoDataOutputQueue = DispatchQueue(label: "video output queue")
+        videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        videoDataOutput.connection(with: .video)?.isEnabled = true
+        
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+        }
         
         // Provide a camera preview
         cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
@@ -79,5 +98,11 @@ extension DtexCameraViewController: AVCapturePhotoCaptureDelegate {
         }
         
         let stillImage = UIImage(data: imageData)
+    }
+}
+
+extension DtexCameraViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // Process frame
     }
 }
